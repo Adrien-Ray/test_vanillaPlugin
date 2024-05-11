3cda145165e7cb2b749bc2010a0194f81eb82732
Adrien-Ray <a.raymond@codeur.online>
2024-05-11 19:57:15 +0200

    fonctionnal minimal plugin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_vanillaPlugin
+
+test enable page : `about:debugging#/runtime/this-firefox`
---
+++ index.js
@@ -0,0 +1,5 @@
+// document.getElementById('search').placeholder = "ceci est un troll";
+
+// document.body.innerHTML = '';
+
+document.body.style.border = "5px solid red";
